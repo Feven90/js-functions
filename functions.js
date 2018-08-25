d9014df2985cddc5553d10a303fbafac9fa49b74
Feven Mulugeta <f.mulugeta90@gmail.com>
2018-08-25 11:52:40 -0500

    functions

--- functions.js
+++ functions.js
@@ -20,3 +20,19 @@ const bearSticks = nuggetizer('bear');
 console.log(nomnom('Feven', bearSticks));
 console.log(nomnom('Feven', 'juice'));
 
+// const Add = (number1) => {
+//     return number1 + 3;
+// }
+// console.log('number1: ', Add(22));
+
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+}
+numberAdder(22);
+
